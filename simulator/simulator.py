14a0e9f8013ea51b33d0a8e3f3f46e76c65aba9b
Manish Regmi <manish.regmi@siu.edu>
2025-12-04 11:50:39 -0600

    fix: ran through simulations and sanity checked those

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -32,7 +32,9 @@ class CloudEdgeSimulator:
         # Determine which nodes are on the cloud for this action
         cloud_nodes = np.where(current_action[:, 1] == 1)[0]
         congestion = abs(self.profiling.get_max_layer_cloud_time(layer) * (self.profiling.numberOfEdgeDevice - 1) * np.random.uniform(0.1,0.5))
-        # congestion = 0.0
+        congestion = 0.0
+        # congestion = abs(self.profiling.get_max_layer_cloud_time(layer) * (self.profiling.numberOfEdgeDevice - 1) * 0.25)
+
         new_cloud_pending = 0.0
         new_cloud_pending += congestion
 
@@ -64,7 +66,7 @@ class CloudEdgeSimulator:
         
         # Bandwidth update (stochastic)
         bw_change = np.random.normal(-0.5, 0.5)
-        # bw_change = 0
+        bw_change = 0
         new_bandwidth = max(1.0, min(bandwidth + bw_change, 15.0))
 
         # Next layer / terminal flag
@@ -150,7 +152,7 @@ class CloudEdgeSimulator:
 
         # --- Completion Time (seconds) ---
         completion_time_s = edge_total_time_s + max_transmission_time + actual_idle_time_s
-        # print(f"Layer {layer} | cloud waiting time: {cloud_pending_ms} | Total Time: {completion_time_s*1000:.2f} ms | Energy: {total_energy:.4f} J, action: {current_action[:,1].tolist()}, bandwidth:, {bandwidth}")
+        print(f"Layer {layer} | cloud waiting time: {cloud_pending_ms} | Total Time: {completion_time_s*1000:.2f} ms | Energy: {total_energy:.4f} J, action: {current_action[:,1].tolist()}, bandwidth:, {bandwidth}")
 
         return total_energy, completion_time_s
 
