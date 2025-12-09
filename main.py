38d92ab926aae0385b639da746737ad8be6f31f5
Manish Regmi <manish.regmi@siu.edu>
2025-12-09 14:56:17 -0600

    feat: add params to record time stamp

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    episodes = 100000
-    # episodes = 1
+    # episodes = 100000
+    episodes = 1
     max_steps = 10
     deadlines = list(range(500,505,5 ))  # 1ms to 700ms
 
