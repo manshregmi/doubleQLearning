a56dcd97463032be914d9a898ccd79cda7515cda
Manish Regmi <manish.regmi@siu.edu>
2026-03-17 15:22:08 -0500

    change initialization fo states varables in PPO

--- simulator/ppo_simulator.py
+++ simulator/ppo_simulator.py
@@ -118,6 +118,7 @@ def run_ppo_simulation(
                         cloud_execution_stats[key] += 1
 
             bandwidth = next_state[0]
+            cloud_time = next_state[1]
 
             prev_neg = current_state[5]
             neg_increased = neg_count > prev_neg
